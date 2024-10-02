33e0f1c7f53014fcd31b1d2eeb243742866bd1bc
Madelyn Mathai <madelynmathai@Madelyns-MacBook-Air.local>
2024-10-02 12:18:59 -0400

    Added bioguide extract method and sponsored bills API call

--- contrans.py
+++ contrans.py
@@ -101,3 +101,58 @@ class contrans:
         #bio_df = bio_df[['name', 'state', 'district', 'partyName', 'bioguideId']]
         return bio_df
      
+    
+    def get_bioguide(self, name, state=None, district=None):
+        """
+        Retrieves the bioguide ID of a member of Congress based on their name, state, and district.
+
+        Parameters:
+            name (str): The name of the member of Congress.
+            state (str, optional): The state of the member of Congress. Defaults to None.
+            district (str, optional): The district of the member of Congress. Defaults to None.
+
+        Returns:
+            str: The bioguide ID of the member of Congress.
+        """
+        members = self.get_bioguideIDs() # replace with a SQL query once we have it as a SQL table
+
+        members['name'] = members['name'].str.lower().str.strip()
+        name = name.lower().strip()
+
+        tokeep = [name in x for x in members['name']] #returning only the ones that match our search
+        members = members[tokeep]
+
+        if state is not None:
+            members = members.query('state == @state')
+        if district:
+            members = members.query('district == @district') 
+        
+        return members.reset_index(drop=True)
+    
+    def get_sponsoredlegislation(self, bioguideid):
+        params = {'api_key': self.congresskey,
+                          'limit': 1} 
+        
+        headers = self.make_headers()
+        root = 'https://api.congress.gov/v3'
+        endpoint = f'/member/{bioguideid}/sponsored-legislation'
+        
+        r = requests.get(root + endpoint,
+                                 params=params,
+                                 headers=headers)
+        totalrecords = r.json()['pagination']['count']
+                
+        params['limit'] = 250
+        j = 0
+        bio_df = pd.DataFrame()
+        while j < totalrecords:
+            params['offset'] = j
+            r = requests.get(root + endpoint,
+                                         params=params,
+                                         headers=headers)
+            records = r.json()['sponsoredLegislation']
+            bills_dict = bills_dict.update(records)
+            j = j + 250
+
+        return bills_dict
+    
